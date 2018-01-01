ba01411bee10d76140c38a28948e2fad8eaa8613
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-01-01 22:26:48 +0900

    時間を表示できるように修正

--- app/src/main/java/com/example/kitchentimer/MainActivity.java
+++ app/src/main/java/com/example/kitchentimer/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.kitchentimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView timerView;
+
+    private SimpleDateFormat dataFormat =
+            new SimpleDateFormat("mm:ss.SSS", Locale.US);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerView = (TextView)findViewById(R.id.timerView);
+        timerView.setText(dataFormat.format(0));
+    }
+
+    public void onStart(View view) {
+        timerView.setText("スタート");
     }
 }
